8d9a05ee67d3df706dc7a26bfa08ac57b5f89eda
EC2 Default User <ec2-user@ip-172-31-16-185.ec2.internal>
2025-07-01 16:53:53 +0000

    Initial commit or update

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple JavaScript to display an alert when the page loads
+window.onload = function() {
+    console.log("Page loaded successfully.");
+    // Uncomment the next line to show alert
+    // alert("Welcome to the Simple CI/CD Demo!");
+};
+
